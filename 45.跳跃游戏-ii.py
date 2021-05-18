a8b0320c03f620492eecf1ea51d3bf4e1e56c8af
宿禹昌 <45455052+Yuri-Su@users.noreply.github.com>
2021-05-18 19:54:32 +0800

    master

--- 45.跳跃游戏-ii.py
+++ 45.跳跃游戏-ii.py
@@ -3,9 +3,21 @@
 #
 # [45] 跳跃游戏 II
 #
-
+# 贪心
 # @lc code=start
 class Solution:
     def jump(self, nums: List[int]) -> int:
-# @lc code=end
 
+        step, end, maxP = 0, 0, 0
+        n = len(nums)
+        for i in range(n - 1):
+            maxP = max(maxP, i + nums[i])
+            # nums[i]记录往后走的步数
+            # 当i == end时，刚好记录了下一步中跳到最远的i
+            if i == end:
+                end = maxP
+                step += 1
+        return step
+
+
+# @lc code=end
